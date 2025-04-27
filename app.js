54a0a57c327caa7b451dc11e6112b7b0db600d15
imdadulhussain8496 <imdadul8496@gmail.com>
2025-04-27 12:44:17 +0530

    add age feater

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 
+let msg = document.querySelector("#msg");
+let btn = document.querySelector("#P");
+
+btn.addEventListener("click", function () {
+  const age = prompt("Please enter your age:");
+
+  if (age >= 18) {
+    window.location.href = "notice.html";
+
+   
+
+    
+  } else {
+    msg.innerText = "Sorry you can not enter. You must be 18+";
+
+    msg.style.transform = "translate(-2%,-10%) scale(1)";
+    msg.style.opacity = 0.8;
+
+    setTimeout(function () {
+      msg.style.opacity = 0;
+      msg.style.display = 'none'
+    }, 4000);
+  }
+
+  
+});
